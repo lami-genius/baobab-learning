a2d8a58a1932abf4a215d8995d05214b00dd535f
lami-genius <fonyuyborislami@gmail.com>
2022-06-10 09:40:34 +0200

    Completed lessons on print statements and comments

---
+++ basic-python-workshop/module-1/print.py
@@ -0,0 +1,48 @@
+# Print an empty line
+print()
+
+# Print Hello World!
+print("Hello World!")
+print()
+
+# Print "Hello World!"
+print('"Hello World!"'),
+print()
+
+# Print and follow to next line
+print(
+    """I'm learning python.
+And we're starting with the print statement"""
+)
+print()
+
+# Using \ to print content in multiple lines
+print(
+    "I'm learning python.\
+ And we're starting with the print statement"
+)
+print()
+
+# The use of end
+print("We'll first learn how to print.", end=" ### ")
+print("Then we'll learn how to comment code.")
+print()
+
+# The use of comments using # or """ """
+"""
+Multiple line comment
+"""
+print("Can you see me?")
+print("Enchante mademoiselle")
+print()
+
+# Single line comment
+
+# Print a string n times
+print("-" * 75)
+print()
+
+# Concatenate in print
+print("Can you see me?", 4)  # space added
+print("Can you see me? " + str(4))  # no space added
+print()
